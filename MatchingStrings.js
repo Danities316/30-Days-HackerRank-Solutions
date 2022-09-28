7fe95d544c14e2a180100b145f0b156fe5ff77c6
danities316 <attayi_316@hotmail.com>
2022-09-28 21:56:02 +0100

    added day 4 of 30 of DSA

---
+++ MatchingStrings.js
@@ -0,0 +1,29 @@
+// There is a collection of input strings and a collection of query strings. For each query string, 
+// determine how many times it occurs in the list of input strings. Return an array of the results.
+
+// Example
+// strings = ['ab', 'ab', abc ']
+// queries = ['ab', 'abc', 'bc ']
+// There are 2 instances of ab', 1  of 'abc' and 0 of 'bc'. For each query, add an element to the return array, result = [2,1,0].
+//Quetion Link: https://bit.ly/3E0uWL6
+//Answer Link: https://bit.ly/3E0uWL6
+
+
+function matchingStrings(strings, queries) {
+    // Write your code here
+    let result = []
+    for(let i of queries){
+        let count = 0
+        for(let j of strings){
+            if(i === j){
+                count += 1
+            }
+        }
+        result.push(count)
+    }
+   
+    return result
+}
+let queries = ['ab', 'abc', 'bc']
+let string = ['ab', 'ab', 'abc']
+console.log(matchingStrings(string, queries))
--- timeConversion.js
+++ timeConversion.js
@@ -1,9 +1,8 @@
-// Questions: https://bit.ly/3UKkWeS
-// Solutions
 // Given a time in -hour AM/PM format, convert it to military (24-hour) time.
 // Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
 // - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
 // Question Link: shorturl.at/cu068
+// Solution Link: https://bit.ly/3rl0vaR
 function timeConversion(s) {
     // Write your code here
     s = s.split(':');
