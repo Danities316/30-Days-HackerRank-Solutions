c257c265bab8470998be4cf5d25bf7cb0e95db33
danities316 <attayi_316@hotmail.com>
2022-09-29 10:28:41 +0100

    added day 5 of 30 of DSA

---
+++ lonelyinteger.js
@@ -0,0 +1,24 @@
+// Question Link: shorturl.at/ituW2
+// Solution Link:
+
+// Given an array of integers, where all elements but one occur twice, 
+// find the unique element.
+
+// Example
+// a = [1,2,3,4,3,2,1]
+// The unique element is 4
+
+
+function lonelyinteger(a) {
+    // Write your code here
+   a.sort((a,b) => {return a-b})
+   
+  for(let i = 0; i < a.length; i += 2){
+    if(a[i] != a[i + 1]){
+        return a[i]
+    }
+  }
+}
+a = [1,2,3,4,3,2,1]
+console.log(lonelyinteger(a))
+//Output 4
